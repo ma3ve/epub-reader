6380472f7d9b473e93174d9afef44c3c7ebd2023
ma3ve <mujammilkhan321@gmail.com>
2022-03-24 02:33:52 +0530

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 function IndexPage() {
-	return <div>Hello World - Typescript - Express</div>;
+	return <h1 className="text-3xl font-bold underline">
+		Hello world!
+	</h1>;
 }
 
 export default IndexPage;
